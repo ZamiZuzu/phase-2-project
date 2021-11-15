98d8e850f7246b4132d487429cd6242c2a92aa3b
Obie <obiemunozjr@gmail.com>
2021-11-15 16:49:07 -0500

    added testing api code

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,22 @@
+import { API_KEY, BASE_URL } from './API';
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [artObjects, setArtObjects] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/object?size=1&yearmade=2002&hasimage=1&apikey=${API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        setArtObjects(data.records);
+      });
+  }, []);
+
+  console.log(artObjects);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Harvard Art Museums</h1>
     </div>
   )
 }
