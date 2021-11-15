8a6f1a9c7701a79d689ae38ecee431d9223d139c
Obie <obiemunozjr@gmail.com>
2021-11-15 14:08:01 -0500

    Added API.js with key and API information

---
+++ src/components/API.js
@@ -0,0 +1,34 @@
+// API Documentation:
+// https://github.com/harvardartmuseums/api-docs
+//
+// Request Structure:
+// https://api.harvardartmuseums.org/RESOURCE_TYPE?apikey=YOUR_API_KEY
+//
+// AVAILABLE RESOURCES:
+// Object
+// Person
+// Exhibition
+// Publication
+// Gallery
+// Spectrum
+// Classification
+// Century
+// Color
+// Culture
+// Group
+// Medium
+// Period
+// Place
+// Technique
+// Worktype
+// Activity
+// Site
+// Video
+// Image
+// Audio
+// Annotation
+
+const API_KEY = "89ce3864-c616-46cd-975c-c1ddb256d505";
+const BASE_URL = "https://api.harvardartmuseums.org"
+
+export { API_KEY, BASE_URL };
