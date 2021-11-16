7ed74d82c22ccc6abf2124b4dcc245d5d58415dd
Obie <obiemunozjr@gmail.com>
2021-11-16 09:33:30 -0500

    Cleaned out deprecated code, updated image placeholder code

--- src/components/ObjectCard.js
+++ src/components/ObjectCard.js
@@ -2,10 +2,14 @@ import React from 'react'
 import * as image from '../images/placeholder.png'
 
 function ObjectCard({ record }) {
+    console.log(record.primaryimageurl)
+    console.log(typeof record.primaryimageurl)
+    if (record.primaryimageurl === undefined || record.primaryimageurl === null) record.primaryimageurl = image.default;
+
     return (
         <div class="ui card">
             <div class="image">
-                <img src={record.primaryimageurl !== undefined ? record.primaryimageurl : image.default} alt="image" />
+                <img src={record.primaryimageurl} alt="image" />
             </div>
             <div class="content">
                 <h3 class="header">{record.title}</h3>
