03b351ed465c7c80cc51bc742a1b07672f3cea4c
Christopher Valle <chrisbv@live.com>
2021-11-15 22:02:33 -0600

    Just testing stuff

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,24 @@
+import React, { useEffect, useState } from "react";
+import {API_KEY, BASE_URL} from "./API.js";
+
 function App() {
+  const [artObjects, setArtObjects] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/object?period=5135&size=483&apikey=${API_KEY}`)
+      .then(res => res.json())
+      .then(data => {
+        setArtObjects(data);
+      });
+  }, []);
+
+  console.log(artObjects);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Harvard Art</h1>
+      <h2></h2>
+      <img src={artObjects.primaryimageurl}/>
     </div>
   )
 }
